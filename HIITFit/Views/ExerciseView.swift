cc4bc454a40843b2822588a859c71f32486876f0
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-09-24 08:40:08 -0400

    Modified Timer

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -41,17 +41,25 @@ struct ExerciseView: View {
     var exercise: Exercise {
       Exercise.exercises[index]
     }
-    let interval: TimeInterval = 30
+    @State private var timerDone = false
+    @State private var showTimer = false
+
     var lastExercise: Bool {
       index + 1 == Exercise.exercises.count
     }
     
     var startButton: some View {
-      Button("Start Exercise") { }
+        
+        Button("Start Exercise") {
+          showTimer.toggle()
+        }
+
     }
 
     var doneButton: some View {
         Button("Done") {
+            timerDone = false
+            showTimer.toggle()
           if lastExercise {
             showSuccess.toggle()
           } else {
@@ -64,6 +72,7 @@ struct ExerciseView: View {
     @State private var showHistory = false
     @State private var showSuccess = false
     
+    
     var body: some View {
         GeometryReader { geometry in
             VStack {
@@ -74,20 +83,28 @@ struct ExerciseView: View {
                 VideoPlayerView(videoName: exercise.videoName)
                               .frame(height: geometry.size.height * 0.35)
                               .padding(20)
-                Text(Date().addingTimeInterval(interval), style: .timer)
-                  .font(.system(size: geometry.size.height * 0.07))
                 HStack(spacing: 150) {
-                    startButton
-                    doneButton
-                        .sheet(isPresented: $showSuccess) {
-                            SuccessView(selectedTab: $selectedTab)
-                                .presentationDetents([.medium, .large])
-                        }                }
-                  .font(.title3)
-                  .padding()
-                RatingView(rating: $rating)
-                  .padding()
+                  startButton
+                  doneButton
+                  .disabled(!timerDone)
+                  .sheet(isPresented: $showSuccess) {
+                    SuccessView(selectedTab: $selectedTab)
+                      .presentationDetents([.medium, .large])
+                  }
+                }
+                .font(.title3)
+                .padding()
+
+                if showTimer {
+                  TimerView(
+                    timerDone: $timerDone,
+                    size: geometry.size.height * 0.07
+                  )
+                }
                 Spacer()
+                RatingView(rating: $rating) // Move RatingView below Spacer
+                  .padding()
+
                 Button("History") {
                   showHistory.toggle()
                 }
